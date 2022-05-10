d40e690e4647a5c74878811cfaf7a9a12dfbea26
Dan Lorenz Altaras <danlorenz06>
2022-05-10 22:26:35 +0800

    added factorial and base power method

--- Calculator.java
+++ Calculator.java
@@ -20,7 +20,24 @@ public class factorial {
         System.out.println("Factorial of "+number+" is: "+fact);
     }
 
-    public int equals(int value, int i) {
+    public int equals(int value, int i) 
+
+public static int basepowerexp(int base, int exp) {
+        if (exp == 0) {
+            return 1;
+        } else {
+            return base * basepowerexp(exp - 1, base);}}
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Base: ");
+        int base = Integer.parseInt(in.nextLine());
+        System.out.println("Enter Base: ");
+        int exp = Integer.parseInt(in.nextLine());
+        System.out.println("The answer to " + base + " to the power of " + exp
+                + " is " + basepowerexp(base, exp));}}
+
+   public int equals(int value, int i) {
         return 0;
     }
-}
+
+//https://github.com/danlorenz06/ecpe205-unit-test.git
